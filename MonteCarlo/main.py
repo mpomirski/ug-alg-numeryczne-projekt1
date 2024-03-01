416add6f2473d972082952374b0e391dcd105d64
Konrad Kreczko <konradkreczko@gmail.com>
2024-03-01 09:56:37 +0100

    feat: Created cool table

--- MonteCarlo/main.py
+++ MonteCarlo/main.py
@@ -1,17 +1,26 @@
 import math
 import random
+import pandas as pd
+from terminaltables import AsciiTable
 
-pi = math.pi
+PI = math.pi
 
-# n = int(input("podaj n: "))
-n = 1000000
-counter = 0
+table_data = [
+    ["Wielkosc probki", "Przyblizenie", "Blad wzgledny przyblizenia"]
+]
 
-for i in range(n):
-    x = random.random() 
-    y = random.random()
-    if math.sqrt(x*x + y*y) <= 1:
-        counter += 1
+for n in range(8):
+    counter = 0
+    size = 10 ** n
+    for i in range(size):
+        x_cord = random.random() 
+        y_cord = random.random()
+        if math.sqrt(x_cord * x_cord + y_cord * y_cord) <= 1:
+            counter += 1
+    pi_approximation = 4 * counter / size
+    relative_error = "{:.6f}".format(abs(PI - pi_approximation) / PI) + "%"
+    table_data.append([size, pi_approximation, relative_error])
 
-print(4 * counter / n)
+table = AsciiTable(table_data, "Aproksymacja Pi metoda Monte Carlo")
+print(table.table)
 
